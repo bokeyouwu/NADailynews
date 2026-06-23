ae3e276fd522a66938724da53ed623be086ba0f2
bokeyouwu <bokeyou5_fbh@163.com>
2026-06-23 14:22:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.NA_DAILY_CONFIG = {
-  SUPABASE_URL: 'https://schauwgqscboljjupyry.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://schauwgqscboljjupyry.supabase.co',
   SUPABASE_KEY: 'sb_publishable_YvF7sCi4om8JAfcDk45C7w_DAqnY8ps',
 };
